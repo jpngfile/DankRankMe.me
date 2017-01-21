496832de8017bb05ad756e7c7db8362684ac17a3
Iyvan-C <iyvan.chandran@gmail.com>
2017-01-21 16:38:55 -0500

    React router issue

--- src/App.js
+++ src/App.js
@@ -5,15 +5,34 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+		
+		<div id="root">
+
+			/* Main Layout */
+			<div class="app">
+				<header class="primary-header"></header>
+				<aside class="primary-aside"></aside>
+				<main>
+					/* Search Layout */
+					<div class="search">
+						<header class="search-header"></header>
+					<div class="results">
+
+					/* User List */
+					<ul class="user-list">
+						<li>Dan</li>
+						<li>Ryan</li>
+						<li>Michael</li>
+					</ul>
+				</div>
+			
+				<div class="search-footer pagination"></div>
+			</div>
+
+			</main>
+  </div>
+
+</div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,73 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Router, Route} from 'react-router';
 
+var Home = React.createClass({
+  render: function() {
+    return (<h1>Welcome to the Home Page</h1>);
+  }
+});
+var Users = React.createClass({
+  render: function() {
+    return (<h1>Welcome to the Users Page</h1>);
+  }
+});
+var Widgets = React.createClass({
+  render: function() {
+    return (<h1>Welcome to the Widgets Page</h1>);
+  }
+});
 ReactDOM.render(
-  <App />,
+  <Router>
+	<Route path="/" component={Home} />
+	<Router compontent={SearchLayout}>
+		<Route path="/users" component={Users} />
+		<Route path="/widgets" component={Widgets} />
+	</Router>
+  </Router>,
   document.getElementById('root')
 );
+
+var MainLayout = React.createClass({
+  render: function() {
+    // Note the `className` rather than `class`
+    // `class` is a reserved word in JavaScript, so JSX uses `className`
+    // Ultimately, it will render with a `class` in the DOM
+    return (
+      <div className="app">
+        <header className="primary-header"></header>
+        <aside className="primary-aside"></aside>
+        <main>
+          {this.props.children}
+        </main>
+      </div>
+    );
+  }
+});
+
+var SearchLayout = React.createClass({
+  render: function() {
+    return (
+      <div className="search">
+        <header className="search-header"></header>
+        <div className="results">
+          {this.props.children}
+        </div>
+        <div className="search-footer pagination"></div>
+      </div>
+    );
+  }
+});
+
+var UserList = React.createClass({
+  render: function() {
+    return (
+      <ul className="user-list">
+        <li>Dan</li>
+        <li>Ryan</li>
+        <li>Michael</li>
+      </ul>
+    );
+  }
+});
